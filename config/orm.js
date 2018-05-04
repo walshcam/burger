4c687cd08d535730f6075e61772e4313957184a9
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-04 19:00:24 -0400

    Finish congif setup

---
+++ config/orm.js
@@ -0,0 +1,22 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//orm function
+
+let orm = {
+
+    selectAll: function() {
+
+    },
+
+    insertOne: function() {
+
+    },
+
+    updateOne: function() {
+
+    }
+
+};
+
+module.exports = orm;
