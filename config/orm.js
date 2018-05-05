11c4c7aac7a6081e4043238e855e0c55c12e0912
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-05 10:25:01 -0400

    Finish ORM - Untested

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,5 @@
+import { connect } from "net";
+
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
@@ -5,16 +7,56 @@ var connection = require("../config/connection.js");
 
 let orm = {
 
-    selectAll: function() {
-
+    //*********Used to display all burgers on the screen**********
+    selectAll: function(callback) {
+        let queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            };
+            
+            callback(result);
+        });
     },
 
-    insertOne: function() {
+    //**********Insert a burger from input field************
+    insertOne: function(burger_name, callback) {
+        //Query construction for burgers table
+        let queryString = "INSERT INTO ";
+        queryString += "burgers ";
+        queryString += "(burger_name, devoured) ";
+        queryString += "VALUES ?";
 
+        //Values to be inserted into query above
+        let values = [burger_name, false];
+
+        connection.query(queryString, values, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callback(result);
+        });
     },
 
-    updateOne: function() {
+    //**************Change a burger from Available to Devoured***********
+    updateOne: function(burger_name, callback) {
+        //Query construction for burgers table
+        let queryString = "UPDATE ";
+        queryString += "burgers ";
+        queryString += "SET devoured = 1 ";
+        queryString += "WHERE burger_name = ?";
+ 
+        //Values to be inserted into query above
+        let values = burger_name
 
+        connection.query(queryString, [values], function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callback(result);
+        });
     }
 
 };
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,15 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var cat = require("../models/burger.js");
+
+//************ROUTES********************* */
+
+//Route to Render Initial Page
+router.get("/", function(req, res) {
+    
+})
+
+
+//Route to POST new burger
+
+//Route to UPDATE burger to devoured
